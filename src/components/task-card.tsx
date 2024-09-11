c5a6bf9f12650a7abb57ea536d334f6e4d030ae5
gnandal <egnandal500@yahoo.com>
2024-09-11 02:04:20 +0100

    path patch

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,9 +1,9 @@
-  import { TaskByTimeFrame } from "../types";
+import { TaskByTimeFrame } from "../types";
 
 export const TaskCard = ({title, current, previous}: TaskByTimeFrame) => {
     return (
         <div className={`w-full overflow-hidden flex-1 flex flex-col max-w-sm bg-${title.toLocaleLowerCase().replace(' ', '-')} rounded-lg shadow`}>
-          <img className='flex-1 self-end -my-3 me-4' src={`../assets/images/icon-${title.toLocaleLowerCase().replace(' ', '-')}.svg`} alt="icon-work" />
+          <img className='flex-1 self-end -my-3 me-4' src={`src/assets/images/icon-${title.toLocaleLowerCase().replace(' ', '-')}.svg`} alt="icon-work" />
           <div className='flex flex-col gap-4 text-start max-w-sm bg-dark-blue p-8 rounded-lg'>
             <div className='w-full flex justify-between align-middle font-semibold'>
               <h2>{title}</h2>
